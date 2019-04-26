41fe23a50d090ea7718dbea27f665a97984006b2
cronugs <cronugs@outlook.com>
2019-04-26 10:39:32 +0200

    added queue() and searchData() into a new function getURL and call it with a call back from getSearch()

--- static/js/search.js
+++ static/js/search.js
@@ -1,36 +1,39 @@
-queue()
-    .defer(d3.json, "http://www.dnd5eapi.co/api/monsters/")
-    .await(searchData);
-
-
-
-function getSearch() {
+function getSearch(callback) {
     var searchTerm = document.getElementById("monsterName").value;
     console.log(searchTerm);
     return searchTerm;
 }
 
-
-function searchData(error, data) {
-
-    //data = searchData;
+function getURL(term) {
     
-    console.log(data);
-    
-    var arr = data.results;
-    console.log(arr);
+    queue()
+    .defer(d3.json, "http://www.dnd5eapi.co/api/monsters/")
+    .await(searchData);
 
-//This snippet was found here https://stackoverflow.com/questions/46143452/get-value-from-dictionary-in-javascript-by-key
+    function searchData(error, data) {
 
-    var result = arr.filter(function(element) {
-        return element.name == "getSearch()";
-    });
+        //data = searchData;
+        
+        console.log(data);
+        
+        var arr = data.results;
+        console.log(arr);
     
-    if (result.length > 0) {
-         //we have found a corresponding element
-        var resultURL = result[0].url;
-        console.log(resultURL);
+    //This snippet was found here https://stackoverflow.com/questions/46143452/get-value-from-dictionary-in-javascript-by-key
+    
+        var result = arr.filter(function(element) {
+            return element.name == getSearch();
+        });
+        
+        if (result.length > 0) {
+             //we have found a corresponding element
+            var resultURL = result[0].url;
+            console.log(resultURL);
+        }
+    
     }
 
 }
+
+
 
