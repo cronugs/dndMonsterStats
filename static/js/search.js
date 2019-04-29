7b654866441ca232c0a697544805c207dd322723
cronugs <cronugs@outlook.com>
2019-04-29 10:54:47 +0200

    cleaned up code

--- static/js/search.js
+++ static/js/search.js
@@ -1,87 +1,51 @@
-//This is called from the search function
-
-function getData(url, cb) {
-    //we set a new XMLHttpRequest to the variable xhr
+//This is called from the searchMonsterData function
+function getData(url, cb) { 
     var xhr = new XMLHttpRequest();
-
-    //6. we set onredystatechange to an annon function which tests the status of the data from the API
+    
+    //we set onredystatechange to an anon function which tests the status of the data from the API
     xhr.onreadystatechange = function() {
-        //7. if the ready state is 4 and the status is 200
+        //if the ready state is 4 and the status is 200
         if (this.readyState == 4 && this.status == 200) {
-            //8. we call our callback function in the getData call and parse is as JSON data. The data held in this.responseText
-            //(xhr.responseText) is passed back into the the writeToDocument function.
+            //we call our callback function in the getData call and parse is as JSON data. The data held in this.responseText
+            //(xhr.responseText) is passed back into the the searchMonsterData function.
+            console.log(xhr.responseText);
             cb(JSON.parse(this.responseText));
         }
     };
-    //I'm not sure what these lines do!
+    
     xhr.open("GET", url);
-    xhr.send();
+    xhr.send();   
 }
 
-function searchForMonsterURL(url) {
+function searchMonsterData(url) {
     var searchTerm = document.getElementById("monsterName").value;
+    var monsterData;
+    var result;
+    var monsterURL;
+    
+    
     //call the get data function and give it the url 
     getData(url, function(data) {
         
-        data = data.results;
-        console.log(data);
+        monsterData = data.results;
+        console.log(monsterData);
 
-        var result = data.filter(function(element) {
+         //This snippet was found here https://stackoverflow.com/questions/46143452/get-value-from-dictionary-in-javascript-by-key
+        //filter() creates an array filled with all array elements that pass a test. Here we are using a function to return the element 
+        //that matches our search. It returns an array with just the name and url.
+        result = monsterData.filter(function(element) {
+             
             return element.name == searchTerm;                
         });
         
         if (result.length > 0) {
-             //we have found a corresponding element
-            var resultURL = result[0].url;
-            console.log(resultURL);
-            return resultURL;
-        }
-        indexRef = resultURL.substr(resultURL.lastIndexOf('/') + 1);
-        console.log(indexRef);
-    }) 
+            //console.log(result);
+            //we have found a corresponding element
+           monsterURL = result[0].url;
+           
+       }
+    })
 
-   // searchForMonsterURL(resultURL);
-    
+    console.log(result);
+
 }
-
-
-
-//searchForMonsterURL('http://www.dnd5eapi.co/api/monsters/');
-
-
-
-
-
-
-//function getRefFromURL(term) {
-
-    
-
-    
-     
-    //queue()
-    //.defer(d3.json, "static/json/monsters.json")
-    //.await(searchData);
-
-  //  function searchData(error, data) {
-        
-        
-        
-        
-  //      console.log(arr);
-    
-    //This snippet was found here https://stackoverflow.com/questions/46143452/get-value-from-dictionary-in-javascript-by-key
-    
-        
-  //  return indexRef;
-    
-//}
-
-
-
-
-///function getSearch(callback) {
-//    var searchTerm = document.getElementById("monsterName").value;
-//    console.log(searchTerm);
-//    return searchTerm;
-//}
