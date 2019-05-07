dd00436b08a743786ae88902f04cfab87b1ba120
cronugs <cronugs@outlook.com>
2019-05-07 12:46:45 +0200

    created a function to make an array of the URLs in resaultArr and a loop in  displayMonster that calls getData for each URL and logs each returned monsters stats

--- static/js/search.js
+++ static/js/search.js
@@ -45,62 +45,92 @@ function searchMonsterData(url) {
     //call the get data function and give it the url 
     getData(url, function (data) {
 
+        //monster data is an array of key value objects with the name and url for each monster
         monsterData = data.results;
         console.log(monsterData);
-        
 
-        //write a for each loop that pushes 
-        
+        //here we use our regex to match the search string to names of monsters and return matching elements
         result = monsterData.filter(function (element) {
-            if (element.name.match(reg)) {                
+            if (element.name.match(reg)) {
                 return element;
-            }            
+            }
         });
 
+        //we have found corresponding elements
         if (result.length > 0) {
-            //we have found a corresponding element
+            //create a new variable for our result array. It contains an array of name url key value pairs for monsters that match
+            //our search
             resultArr = result;
             console.log(resultArr);
-            monsterURL = result[0].url;
+            //this takes the url of the first result and puts it in the monsterURL variable
+            // *** This is a problem because we actuall want the url for each returned monster ***
+            monsterURL = resultArr[0].url;
             console.log(monsterURL);
         }
 
+        //we call getData again, this time to return the data for an individual monster
+        // *** this needs to change to take multiple urls and 
         getData(monsterURL, function (data) {
             searchedMonster = data;
-            displayMonster(resultArr, searchedMonster);
+
+            //return an array of key value pairs from searchedMonster
+            var monsterArr = Object.entries(resultArr);
+
+            console.log(monsterArr);
+            console.log(resultArr);
+            //console.log(searchedMonster);       
+
+            //create an array containing the URLS from resultArr
+            function listOfURLS(mons) {
+                let URL_list = [];
+                for (let i = 0; i < mons.length; i += 1) {
+                    URL_list.push(mons[i].url);
+                }
+                return URL_list;
+            }
+
+            //console.log(listOfURLS(resultArr));
+
+            displayMonster(resultArr, listOfURLS(resultArr));
         })
     })
 }
+
 //getTableHeaders takes an object as a parameter, in this case it is the first result returned in our data array which contains
 //an object with two key, value pairs. The keys are name and url. Which will be our table headers
 function getTableHeaders(obj) {
     //create a new array to hold out table headers
     var tableHeaders = [];
 
+    var keys = Object.keys(obj)[0];
+    console.log(keys);
+
     //for each key in our object 
-    Object.keys(obj).forEach(function (key) {
-        //push the html tags enclosing the value of our key
-        tableHeaders.push(`<td>${key}</td>`);
-    });
+    //Object.keys(obj).forEach(function (key) {
+    //push the html tags enclosing the value of our key
+    tableHeaders.push(`<td>${keys}</td>`);
+    //});
     //return the tableHeaders array, further encapsulated in <tr> tags.
     return `<tr>${tableHeaders}</tr>`;
 }
 
-function displayMonster(resultArr, searchedMonster) {
-    
-    //assign out incoming array to the var data
-    data = resultArr;
+function displayMonster(resultArr, monsterURLList) {
+
     //declare a new variable containing an empty array for our table rows
-    var tableRows = []; 
+    var tableRows = [];
+    //console.log(monsterURLList[0]);
+
+    for (let i = 0; i < monsterURLList.length; i += 1) {
+        getData(monsterURLList[i], function (data) {
+            var individualMonsters = [];
+            individualMonsters.push(data)
+            console.log(individualMonsters);
+        })
+    }
     //declare a variable to contain our targeted html element   
     var resultTable = document.getElementById("resultTable");
     //declare a variable for a table headers and pass in the getTableHeaders function with data[0] as an argument
-    var tableHeaders = getTableHeaders(data[0]);
-    //console.log(data);
-    //console.log(data[0]);
-    //console.log(searchedMonster);
-
-    console.log(resultArr);
+    var tableHeaders = getTableHeaders(resultArr[0]);
 
     resultArr.forEach(function (item) {
         var dataRow = [];
@@ -117,41 +147,3 @@ function displayMonster(resultArr, searchedMonster) {
 
 
 }
-
-/*var wrapper = $("#challenge-rating"),
-        container = $('<div id="challenge-rating" class="container"></div>');
-    wrapper.append(container);
-    container.append('<div class="category">' + searchedMonster.challenge_rating + '</div>');
-
-    var abilityWrapper = $("#ability-scores"),
-        container = $('<div id="ability-scores" class="container"></div>');
-    abilityWrapper.append(container);
-    container.append('<div class="category">' + '<br>' + "Intelligence: " + searchedMonster.intelligence + '</div>');
-    container.append('<div class="category">' + "Dexterity: " + searchedMonster.dexterity + '</div>');
-    container.append('<div class="category">' + "Wisdom: " + searchedMonster.wisdom + '</div>');
-    container.append('<div class="category">' + "Strength: " + searchedMonster.strength + '</div>');
-    container.append('<div class="category">' + "Constitution: " + searchedMonster.constitution + '</div>');
-    container.append('<div class="category">' + "Charisma: " + searchedMonster.charisma + '</div>');
-
-    var specialAbilitiesWrapper = $("#special-abilities"),
-        container = $('<div id="special-abilities" class="container"></div>');
-    specialAbilitiesWrapper.append(container);
-    container.append('<div class="category">' + '<br>' + "Special Abilities: " + searchedMonster.special_abilities[0].desc + '</div>');
-    
-} */
-
-
-
-
-
-
-
-
-//This snippet was found here https://stackoverflow.com/questions/46143452/get-value-from-dictionary-in-javascript-by-key
-
-//getData(monsterURL, function (data) {
-//console.log(monsterURL);
-//  console.log(data);
-//  searchedMonster = data;
-//console.log(searchedMonster);
-// })
