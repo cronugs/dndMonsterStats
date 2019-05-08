402f6174f0e237f7b95ebdc89b5b7e0244ba5aad
cronugs <cronugs@outlook.com>
2019-05-08 13:24:42 +0200

    Moved newArray outside the for loop so it updates properly. Wrote a new function buildTables() that takes our final array of search hits so I can start working on them

--- static/js/search.js
+++ static/js/search.js
@@ -61,11 +61,11 @@ function searchMonsterData(url) {
             //create a new variable for our result array. It contains an array of name url key value pairs for monsters that match
             //our search
             resultArr = result;
-            console.log(resultArr);
+            //console.log(resultArr);
             //this takes the url of the first result and puts it in the monsterURL variable
             // *** This is a problem because we actuall want the url for each returned monster ***
             monsterURL = resultArr[0].url;
-            console.log(monsterURL);
+            //console.log(monsterURL);
         }
 
         //we call getData again, this time to return the data for an individual monster
@@ -74,9 +74,9 @@ function searchMonsterData(url) {
             searchedMonster = data;
 
             //return an array of key value pairs from searchedMonster
-            var monsterArr = Object.entries(resultArr);
+            //var monsterArr = Object.entries(resultArr);
 
-            console.log(monsterArr);
+            //console.log(monsterArr);
             console.log(resultArr);
             //console.log(searchedMonster);       
 
@@ -118,15 +118,28 @@ function displayMonster(resultArr, monsterURLList) {
 
     //declare a new variable containing an empty array for our table rows
     var tableRows = [];
-    //console.log(monsterURLList[0]);
+    var newArray = [];
+    var counter = 0;
+    //for each url in monsterURLList
 
     for (let i = 0; i < monsterURLList.length; i += 1) {
+        //call getData for url and get data back
         getData(monsterURLList[i], function (data) {
-            var individualMonsters = [];
-            individualMonsters.push(data)
-            console.log(individualMonsters);
+
+            //add data to newArray 
+            newArray.push(data);
+            //increment counter each time newArray is pushed
+            counter++;
+            //when it has been pushed for each url
+            if (counter === monsterURLList.length) {
+                //call build tables and pass it out newArray
+                buildTables(newArray);
+            }
         })
+
+
     }
+    /*
     //declare a variable to contain our targeted html element   
     var resultTable = document.getElementById("resultTable");
     //declare a variable for a table headers and pass in the getTableHeaders function with data[0] as an argument
@@ -144,6 +157,103 @@ function displayMonster(resultArr, monsterURLList) {
     });
 
     resultTable.innerHTML = `<table>${tableHeaders}${tableRows}</table>`.replace(/,/g, "");
+    */
+    /*
+    function generate_table() {
+        // get the reference for the body
+        var body = document.getElementsByTagName("resultTable")[0];
+
+        // creates a <table> element and a <tbody> element
+        var tbl = document.createElement("table");
+        var tblBody = document.createElement("tbody");
+
+        // creating all cells
+        for (var i = 0; i < 2; i++) {
+            // creates a table row
+            var row = document.createElement("tr");
+
+            for (var j = 0; j < 2; j++) {
+                // Create a <td> element and a text node, make the text
+                // node the contents of the <td>, and put the <td> at
+                // the end of the table row
+                var cell = document.createElement("td");
+                var cellText = document.createTextNode("cell in row " + i + ", column " + j);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+
+            // add the row to the end of the table body
+            tblBody.appendChild(row);
+        }
+
+        // put the <tbody> in the <table>
+        tbl.appendChild(tblBody);
+        // appends <table> into <body>
+        body.appendChild(tbl);
+        // sets the border attribute of tbl to 2;
+        tbl.setAttribute("border", "2");
+    }
+    */
+    //generate_table();
 
 
+}
+
+function buildTables(combinedArray) {
+
+    function tableCreate() {
+        //body reference 
+        var body = document.getElementsByTagName("body")[0];
+      
+        // create elements <table> and a <tbody>
+        var tbl = document.createElement("table");
+        var tblBody = document.createElement("tbody");
+      
+        // cells creation
+        for (var j = 0; j <= combinedArray.length; j++) {
+          // table row creation
+          var row = document.createElement("tr");
+      
+          for (var i = 0; i < 2; i++) {
+            // create element <td> and text node 
+            //Make text node the contents of <td> element
+            // put <td> at end of the table row
+            var cell = document.createElement("td");
+            var cellText = document.createTextNode("cell is row " + j + ", column " + i);
+      
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+          }
+      
+          //row added to end of table body
+          tblBody.appendChild(row);
+        }
+      
+        // append the <tbody> inside the <table>
+        tbl.appendChild(tblBody);
+        // put <table> in the <body>
+        body.appendChild(tbl);
+        // tbl border attribute to 
+        tbl.setAttribute("border", "2");
+    }
+
+    tableCreate();
+
+
+    //just playing around with the data
+    console.log("name: " + combinedArray[3].name);
+    console.log("challenge rating: " + combinedArray[3].challenge_rating);
+    console.log("Size: " + combinedArray[3].size);
+    console.log("Hit points: " + combinedArray[3].hit_points);
+    console.log("\n");
+    console.log("Ability Scores");
+    console.log("Strength: " + combinedArray[3].strength);
+    console.log("Dexterity: " + combinedArray[3].dexterity);
+    console.log("Intelligence: " + combinedArray[3].intelligence);
+    console.log("Wisdom: " + combinedArray[3].wisdom);
+    console.log("Charisma: " + combinedArray[3].charisma);
+
+    console.log("\n");
+    console.log("Other details");
+    console.log("Special ability: " + combinedArray[3].special_ability);
 }
